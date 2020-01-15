7dddf188b57c16104ba4b812ad74b264fd0b5639
JaysonBH <jhairston@vmware.com>
2020-01-15 13:35:31 -0500

    initial commit + LICENSE

---
+++ src/main/java/com/vmware/tanzu/support/scs/refreshmirror/RefreshMirrorApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.tanzu.support.scs.refreshmirror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshMirrorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefreshMirrorApplication.class, args);
+    }
+
+}
